b2cefc3add0072d1469aa3c42c9788b3d6ddc77c
Westnik <VadimSavelyev@users.noreply.github.com>
2017-06-21 01:04:27 +1000

    Update index.js
    no

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var toAdd = 23
 
 function summer (sumVal) {
   return function (toAdd) {
-    if (!toAdd) throw new Error('trying to pull a fast oneS?')
+    if (!toAdd) throw new Error('trying to pull a fast oneS2?')
     return sumVal + toAdd
   }
 }
