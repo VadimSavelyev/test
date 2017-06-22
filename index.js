aa0bb0c1d7c418d22921952f37f009e9e77b23f4
Westnik <>
2017-06-22 13:19:10 +1000

    Three
    Three

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var toAdd = 23
 
 function summer (sumVal) {
   return function (toAdd) {
-    if (!toAdd) throw new Error('trying to pull a fast two?')
+    if (!toAdd) throw new Error('trying to pull a fast three?')
     return sumVal + toAdd
   }
 }
