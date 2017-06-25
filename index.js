94744ab355ba5b77fd5ee0292a104a6169315235
Westnik <>
2017-06-25 21:38:10 +1000

    six
    six

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var toAdd = 23
 
 function summer (sumVal) {
   return function (toAdd) {
-    if (!toAdd) throw new Error('trying to pull a fast five?')
+    if (!toAdd) throw new Error('trying to pull a fast six?')
     return sumVal + toAdd
   }
 }
